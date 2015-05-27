08f5ee3dfe4515c9248a846a07e730ac88c9394b
Martin E. Zulliger <mzulliger@gmail.com>
2015-05-27 13:40:03 +0200

    Added first files

---
+++ pgtest.js
@@ -0,0 +1,118 @@
+/*jslint node: true regexp: true nomen: true */
+'use strict';
+
+var AssertionError = require('assertion-error');
+var _ = require('lodash');
+var rewire = require('rewire');
+
+function Query(args) {
+    var s;
+    
+    if (args.length === 0) {
+        this.regExp = /.*/;
+    } else {
+        if (typeof (args[0]) === 'string') {
+            s = args[0].replace(/\$/g, '\\$').replace(/\*/g, '\\*');
+            this.regExp = new RegExp('^' + s + '$'); //Exact match        
+        } else if (args[0] instanceof RegExp) {
+            this.regExp = args[0];
+        }
+        
+        if (args.length === 2) {
+            this.params = args[1];
+        }
+    }
+}
+
+Query.prototype.run = function (args) {
+    var query, params, cb;
+    
+    query = args[0];
+    if (args.length === 2) {
+        cb = args[1];
+    } else {
+        params = args[1];
+        cb = args[2];
+    }
+
+    if (query.match(this.regExp) === null) {
+        throw new AssertionError('Unexpected query: ' + query);
+    }
+
+    if (!_.isEqual(this.params, params)) {
+        throw new AssertionError('Unexpected params: ' + params);
+    }
+    
+    if (this.retVal !== undefined) {
+        cb(this.retVal.err, {
+            rows: this.retVal.data
+        });
+    } else {
+        cb(null, {rows: []});
+    }
+};
+
+function PgMock() {
+    this.queue = [];
+}
+
+PgMock.prototype.connect = function (db, cb) {
+    var client, done, self;
+        
+    self = this;
+    this.doneCalled = false;
+    
+    client = {
+        query: function (query) {
+            var retVal;
+            
+            if (self.queue.length === 0) {
+                throw new AssertionError('Unexpected query: ' + query);
+            }
+            
+            self.queue.shift().run(arguments);
+        }
+    };
+    
+    done = function () {
+        self.doneCalled = true;
+    };
+    
+    cb(null, client, done);
+};
+
+PgMock.prototype.check = function () {
+    if (!this.doneCalled) {
+        throw new AssertionError('Done was not called');
+    }
+    
+    if (this.queue.length !== 0) {
+        throw new AssertionError('Not all queries were executed');
+    }
+};
+
+PgMock.prototype.expect = function () {
+    var match = new Query(arguments);
+    this.queue.push(match);
+    
+    return {
+        returning: function (err, data) {
+            match.retVal = {
+                err: err,
+                data: data
+            };
+        }
+    };
+};
+
+function setup(modName, varName) {
+    var spy, mod;
+
+    spy = new PgMock();
+    mod = rewire(modName);
+    mod.__set__(varName, spy);
+    return spy;
+}
+
+exports.setup = setup;
+exports.PgMock = PgMock;
